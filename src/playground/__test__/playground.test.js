085f713feef4cdd9be8a9a0a5ce93d6fee19392e
wetech16 <67137441+wetech16@users.noreply.github.com>
2021-06-14 20:31:46 -0700

    -update

--- src/playground/__test__/playground.test.js
+++ src/playground/__test__/playground.test.js
@@ -1,8 +1,47 @@
-import playground from '../playground';
+import playground from "../playground";
 
-describe('playground', () => {
-  it('should return correct results', () => {
+class Stack {
+  constructor() {
+    this.top = -1;
+    this.items = {};
+  }
+  get peek() {
+    return this.items[this.top];
+  }
+
+  push(value) {
+    this.top += 1;
+    this.items[this.top] = value;
+  }
+  topOff() {
+    this.top -= 1;
+  }
+}
+
+describe("playground", () => {
+  it("should return correct results", () => {
     // Replace the next dummy test with your playground function tests.
     expect(playground()).toBe(120);
+  });
+});
+
+describe("test hello", () => {
+  let stack;
+  beforeEach(() => {
+    stack = new Stack();
+  });
+  it("simply test", () => {
+    expect(stack.top).toBe(-1);
+    expect(stack.items).toEqual({});
+  });
+  it("before Each", () => {
+    stack.push("h");
+    expect(stack.top).toBe(0);
+    expect(stack.peek).toBe("h");
+  });
+  it("top off", () => {
+    stack.topOff();
+    expect(stack.top).toBe(-2);
+    expect(stack.peek).toEqual(undefined);
   });
 });
